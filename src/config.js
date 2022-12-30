135268a6476dc7f97681aeeafc6a2f7df70125b8
NabajitS <86390740+NabajitS@users.noreply.github.com>
2022-12-30 18:45:30 +0530

    config changed

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 //Production
-// const config = {
-//     local: "http://localhost:8000",
-//     production: "https://blog-server-llqa.onrender.com"
-// }
+const config = {
+    local: "http://localhost:8000",
+    production: "https://blog-server-llqa.onrender.com"
+}
 
 //Locally(url's are written opposite)
-const config = {
-    local: "https://blog-server-llqa.onrender.com",
-    production: "http://localhost:8000"
+// const config = {
+//     local: "https://blog-server-llqa.onrender.com",
+//     production: "http://localhost:8000"
 
-}
+// }
 
 export default config;
