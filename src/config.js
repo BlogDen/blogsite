7627caa0afce4c4a959810d8641f63338c4ed77d
NabajitS <Nabajitsarmah215@gmail.com>
2024-03-30 20:09:52 +0530

    config update

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
 //Production
+// const config = {
+//     local: "http://localhost:8000",
+//     production: "https://blog-server-llqa.onrender.com"
+// }
+
+// Locally(url's are written opposite)
 const config = {
     local: "http://localhost:8000",
     production: "https://blog-server-llqa.onrender.com"
+    // local: "https://blog-server-llqa.onrender.com",
+    // production: "http://localhost:8000"
 }
-
-//Locally(url's are written opposite)
-// const config = {
-//     local: "https://blog-server-llqa.onrender.com",
-//     production: "http://localhost:8000"
-
-// }
 
 export default config;
